16cd7614c87853be86bbc65e8f289d94b5e3be27
ignaciocha <oesnahn@gmail.com>
2022-10-27 13:56:53 +0900

    부트 연동

--- project01/src/App.js
+++ project01/src/App.js
@@ -8,25 +8,38 @@ import TeamRoom from './pages/TeamRoom';
 import TeamSetting from './pages/TeamSetting';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import NewTeam from './pages/NewTeam';
-import BubbleChart from '@weknow/react-bubble-chart-d3'
 import './styles/Chart.css'
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [hello, setHello] = useState('')
+
+  useEffect(()=>{
+    axios.get('/hello')
+    .then(response => setHello(response.data))
+    .catch(error => console.log(error))
+  },[]);
+
   return (
    <div>
+    
+    백엔드에서 가져온 데이터 입니다 : {hello}
+
+
     <Header/>
     {/* <FilterBox/> */}
     {/* <TeamRoom/> */}
 
-    <Routes>
+    {/* <Routes>
       <Route path='/' element={<Main/>}></Route>
       <Route path='/teamSetting' element={<TeamSetting/>}></Route>
       <Route path='/newTeam' element={<NewTeam/>}></Route>
       <Route path='*'></Route>
       <Route path='*'></Route>
-    </Routes>
+    </Routes> */}
     
-   </div>
+     </div>
   );
 }
 

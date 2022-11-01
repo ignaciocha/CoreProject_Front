58af3d2d64b166d7697dafc6bced21896bd264f0
forourhappy <foroh1504@gmail.com>
2022-11-01 17:21:03 +0900

    필터 DB 카테고리 연결

--- project01/src/components/Filter/FilterBox.js
+++ project01/src/components/Filter/FilterBox.js
@@ -1,45 +1,49 @@
 import React, { useState } from 'react';
-import FilterMenuList from './FilterMenuList';
 import '../../styles/FilterBox.css'
-import { useEffect } from 'react';
 import axios from 'axios'
+import FilterItemList from './FilterItemList';
 
 
 
 const FilterBox = () => {
   
   const [gameDetail, setGameDetail] = useState([]);
-  const [selectGame, setSelectGame] = useState('');
 
-  const gameClick = () => {
-    // 게임 클릭한 이름 선택(소문자)
-    setSelectGame('lol')
-  }
-  
-  const applyFilter = async () => {
-    await axios.get('/api/filter', {
+  /** 게임 클릭하면 카테고리에 맞는 필터 보여주기 */
+  const gameClick = (e) => {
+    // 클릭한 게임 이름 선택(소문자)
+    const selectGame = e.target.value;
+    axios.get('/api/filter', {
       params: {
         game: selectGame
       }
     })
-    .then(e => {console.log('성공 ',e);})
+    .then(e => {
+      // 키 이름 변경
+        let newItem = {};
+        newItem['포지션'] = e.data['position'];
+
+        if(e.data['dungeon'] !== undefined) {
+        newItem['던전'] = e.data['dungeon'];
+        }else {
+          newItem['티어'] = e.data['tier'];
+        }
+        setGameDetail(newItem);
+        console.log(newItem);
+    })
     .catch(e => {console.log('필터 에러 :',e);})
   }
-  
-  const filterMenuList = Object.entries(gameDetail).map((i, idx) => (
-    <FilterMenuList item={i} key={idx}/>
-  ))
-
   
 
   return (
     <div className='filterBoxStyle'>
-      {filterMenuList}
-      <button onClick={gameClick}>여기!</button>
+      <FilterItemList item={gameDetail} />
+      <button onClick={gameClick} value='lol'>여기!</button>
+      {/* {filterMenuList}
       <div className='filterDoneBox'>
-      <button className="filterDone" onClick={applyFilter}>적용</button>
+      <button className="filterDone">적용</button>
       <button className="filterReset">초기화</button>
-      </div>
+      </div> */}
     </div>
   )
 }
--- project01/src/components/Filter/FilterItemList.js
+++ project01/src/components/Filter/FilterItemList.js
@@ -1,24 +1,57 @@
-import React, { useState } from 'react';
-import Button from '../Button';
+import React, { useState } from "react";
 
-const FilterItemList = (props) => {
+const FilterItemList = ({ item }) => {
 
-  const [toggle, setToggle] = useState(false);
-  const clickedToggle = () => {
-    setToggle((prev) => !prev);
+  // const [toggle, setToggle] = useState(false);
+  // const clickedToggle = () => {
+  //   setToggle((prev) => !prev);
+  // };
+
+  const [filterList, setFilterList] = useState([]);
+
+
+  // 필터링 리스트에 요소 추가
+  const addFilterList = e => {
+    if(filterList.includes(e.target.value)) {
+      const newFilterList = filterList.filter(i => (
+        i !== e.target.value
+      ))
+      setFilterList(newFilterList)
+    }else {
+      const newFilterList = filterList.concat(e.target.value)
+      setFilterList(newFilterList)
+    }
   }
-  
 
-  
+  // 필터 리스트 불러오기
+  const filterItem = Object.entries(item).map((i) => {
+    return (
+      <div className="gameFilterItemList">
+        <h2 className="gameFilterMenu">{i[0]}</h2>
+        {i[1].map((j) => (
+          <label className="filterCheck">
+            <input type="checkbox" value={j} onClick={addFilterList} />
+            <span className="gameFilterItem">{j}</span>
+          </label>
+        ))}
+      </div>
+    );
+  });
+
+
+
   return (
-    <div className='buttonStyle'>
-      <Button type='filter'
+    <div className="buttonStyle">
+      
+      {filterItem}
+
+      {/* <Button type='filter'
       onClick={clickedToggle}
       toggle={toggle}
       children={props.item}>
-      </Button>
+      </Button> */}
     </div>
-  )
-}
+  );
+};
 
 export default FilterItemList;
